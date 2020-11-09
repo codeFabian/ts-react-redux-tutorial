952658401d80c8bb52ebf795576942ee51add5d4
codeFabian <yolo.fabian.k@gmail.com>
2020-11-09 17:45:37 +0900

    :fire: 3 tabs practice using hooks with typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-
+import React from "react"
+import Tabs from "./Tabs"
+import Tab from "./Tab"
 
 function App() {
   return (
-    <div className="App">
-      <h1>fabian</h1>
-    </div>
-  );
+    <Tabs>
+      <Tab title="수강생정보">이안에 데이터 잔뜩 111111111</Tab>
+      <Tab title="기수이동">이안에 데이터 잔뜩 222222222</Tab>
+      <Tab title="과제현황">이안에 데이터 잔뜩 333333333</Tab>
+    </Tabs>
+  )
 }
 
 export default App;
